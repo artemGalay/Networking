7985e4c4f832b77a82d3bdd960da81560f6a3012
Artem Galay <iostema@mail.ru>
2022-10-23 23:01:58 +0300

    Add downloadPostsButton and create url.

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,7 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var downloadPostsButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "download")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(downloadPostsTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(downloadPostsButton)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            downloadPostsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            downloadPostsButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            downloadPostsButton.widthAnchor.constraint(equalToConstant: 200),
+            downloadPostsButton.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+
+    @objc func downloadPostsTapped() {
+
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+            } else if let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      let data = data,
+                      let dataAsString = String(data: data, encoding: .utf8) {
+                print(response)
+                print("statusCode: \(response.statusCode)")
+                print("data: \(dataAsString)")
+            }
+        }.resume()
     }
 }
